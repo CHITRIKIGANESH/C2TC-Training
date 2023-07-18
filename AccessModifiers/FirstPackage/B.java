f6dda053d95f8a3cedc448494421b763196b238d
CHITRIKIGANESH <ganiganesh210@gmail.com>
2023-07-18 15:44:53 +0530

    Added second package

--- AccessModifiers/FirstPackage/B.java
+++ AccessModifiers/FirstPackage/B.java
@@ -1,8 +1,8 @@
 package FirstPackage;
 
-public class B {
+public class B extends A{
     public static void main(String[] args){
-        A b = new A();
+        B b = new B();
         System.out.println(b.name);
         System.out.println(b.email_id);
         System.out.println(b.roll_no);
