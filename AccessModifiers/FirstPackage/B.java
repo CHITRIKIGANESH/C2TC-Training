c2c5b28c73907ede0db0c42d78e53f348bfc100a
CHITRIKIGANESH <ganiganesh210@gmail.com>
2023-07-18 14:07:14 +0530

    First commit

---
+++ AccessModifiers/FirstPackage/B.java
@@ -0,0 +1,10 @@
+package FirstPackage;
+
+public class B {
+    public static void main(String[] args){
+        A b = new A();
+        System.out.println(b.name);
+        System.out.println(b.email_id);
+        System.out.println(b.roll_no);
+    }
+}
