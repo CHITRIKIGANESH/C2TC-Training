0a03ddd8367fec592724c242836494500a914d79
CHITRIKIGANESH <ganiganesh210@gmail.com>
2023-07-18 23:31:38 +0530

    Access Modifiers

--- AccessModifiers/FirstPackage/A.java
+++ AccessModifiers/FirstPackage/A.java
@@ -6,4 +6,12 @@ public class A {
     String email_id = "ganiganesh210@gmail.com";
     protected int roll_no = 55;
 
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.roll_no);
+        System.out.println(a.name);
+        System.out.println(a.email_id);
+        System.out.println(a.PhoneNumber);
+    }
+
 }
