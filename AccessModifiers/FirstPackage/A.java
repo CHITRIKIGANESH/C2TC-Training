117d185044af8bf972af62005db6e63b98e643fc
CHITRIKIGANESH <ganiganesh210@gmail.com>
2023-07-18 22:52:27 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	AccessModifiers/FirstPackage/B.java

---
+++ AccessModifiers/FirstPackage/A.java
@@ -0,0 +1,9 @@
+package FirstPackage;
+
+public class A {
+    public String name = "Ganesh";
+    private int PhoneNumber = 1234567820;
+    String email_id = "ganiganesh210@gmail.com";
+    protected int roll_no = 55;
+
+}
