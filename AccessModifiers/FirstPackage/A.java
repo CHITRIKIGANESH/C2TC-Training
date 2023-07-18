1511ee8af6d84bc1b355b77991e885c93803fcb8
Chitriki Ganesh <104210702+CHITRIKIGANESH@users.noreply.github.com>
2023-07-18 22:53:23 +0530

    Delete AccessModifiers/FirstPackage directory

--- AccessModifiers/FirstPackage/A.java
+++
@@ -1,9 +0,0 @@
-package FirstPackage;
-
-public class A {
-    public String name = "Ganesh";
-    private int PhoneNumber = 1234567820;
-    String email_id = "ganiganesh210@gmail.com";
-    protected int roll_no = 55;
-
-}
