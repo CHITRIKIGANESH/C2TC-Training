83e21a1097ef6bc6f92a0e76d5a91f7060fcc01d
Chitriki Ganesh <104210702+CHITRIKIGANESH@users.noreply.github.com>
2023-07-18 23:18:57 +0530

    Access Modifiers

---
+++ AccessModifiers/SecondPackage/C.java
@@ -0,0 +1,10 @@
+package SecondPackage;
+import FirstPackage.A;
+
+public class C extends A {
+    public static void main(String[] args) {
+        C c = new C();
+        System.out.println(c.name);
+        System.out.println(c.roll_no);
+    }
+}
